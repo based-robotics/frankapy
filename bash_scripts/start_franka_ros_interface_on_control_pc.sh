75067a6349addf9642fc3e1cb0616a001cecbc3f
Based robot <github@irislab.dooray.com>
2025-06-02 16:45:30 +0900

    feat: update scripts to run with local setup

--- bash_scripts/start_franka_ros_interface_on_control_pc.sh
+++ bash_scripts/start_franka_ros_interface_on_control_pc.sh
@@ -20,19 +20,19 @@ else
 if [ "$control_pc_use_password" = "0" ]; then
 ssh -tt $control_pc_uname@$control_pc_ip_address << EOSSH
 cd $control_pc_franka_interface_path
-cd ros2_ws
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/$control_pc_uname/$control_pc_franka_interface_path/build/franka-interface-common:/home/$control_pc_uname/$control_pc_franka_interface_path/build/franka-interface/proto
-source install/setup.bash
-ros2 launch franka_ros_interface franka_ros_interface.launch.py robot_num:=$robot_number
+bash
+source $rosmaster_path $control_pc_ip_address $workstation_ip_address
+source $catkin_ws_setup_path
+roslaunch franka_ros_interface franka_ros_interface.launch robot_num:=$robot_number
 bash
 EOSSH
 else
 sshpass -p "$control_pc_password" ssh -tt -o StrictHostKeyChecking=no $control_pc_uname@$control_pc_ip_address << EOSSH
 cd $control_pc_franka_interface_path
-cd ros2_ws
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/$control_pc_uname/$control_pc_franka_interface_path/build/franka-interface-common:/home/$control_pc_uname/$control_pc_franka_interface_path/build/franka-interface/proto
-source install/setup.bash
-ros2 launch franka_ros_interface franka_ros_interface.launch.py robot_num:=$robot_number
+bash
+source $rosmaster_path $control_pc_ip_address $workstation_ip_address
+source $catkin_ws_setup_path
+roslaunch franka_ros_interface franka_ros_interface.launch robot_num:=$robot_number
 bash
 EOSSH
 fi
